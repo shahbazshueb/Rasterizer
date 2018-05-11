0637aab721ea7e80bd6cbc72a7d3ad8bbc81eebf
shahbazshueb-10p <shahbaz.ali@tenpearls.com>
2018-05-11 19:48:17 +0500

    Working on algorithm

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace rasterizer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var points = GetPointsOnLine(2.1F,0.3F,3,15);
+            var list = new List<Point>();
+            foreach(var point in points) {
+                Console.WriteLine(point);
+            }
+            
+        }
+        static void line(int x1, int y1, int x2, int y2)
+        {
+            int w = x2 - x1;
+            int h = y2 - y1;
+            int x = x1;
+            int y = y1;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest && (x != x2 || y != y2); i++)
+            {
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
+                Console.WriteLine("x:" + x + " Y: " + y);
+            }
+}
+
+        public static IEnumerable<PointF> GetPointsOnLine(float x0, float y0, float x1, float y1)
+        {
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                float t;
+                t = x0; // swap x0 and y0
+                x0 = y0;
+                y0 = t;
+                t = x1; // swap x1 and y1
+                x1 = y1;
+                y1 = t;
+            }
+            if (x0 > x1)
+            {
+                float t;
+                t = x0; // swap x0 and x1
+                x0 = x1;
+                x1 = t;
+                t = y0; // swap y0 and y1
+                y0 = y1;
+                y1 = t;
+            }
+            float dx = x1 - x0;
+            float dy = Math.Abs(y1 - y0);
+            float error = dx / 2;
+            float ystep = (y0 < y1) ? 1 : -1;
+            float y = y0;
+            for (float x = x0; x <= x1; x++)
+            {
+                yield return new PointF((steep ? y : x), (steep ? x : y));
+                error = error - dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+            yield break;
+        }
+    }
+}
